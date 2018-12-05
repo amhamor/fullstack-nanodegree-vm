c38cc138c860edc30f08cdb224704c8e28f16c8b
amhamor <amhamor@gmail.com>
2018-12-04 23:55:35 -0600

    Updated Python code with Python objects that fills in restaurant_menu.db

--- vagrant/database_setup.py
+++ vagrant/database_setup.py
@@ -14,14 +14,23 @@ Base = declarative_base()
 
 
 class Restaurant(Base):
-    __table_name__ = "restaurant"
+    __tablename__ = "restaurant"
 
     name = Column(String(80), nullable=False)
     id = Column(Integer, primary_key=True)
 
 
 class MenuItem(Base):
-    __table_name__ = "menu_item"
+    __tablename__ = "menu_item"
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    course = Column(String(250))
+    description = Column(String(250))
+    price = Column(String(8))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+
+    restaurant = relationship(Restaurant)
 
 
 engine = create_engine('sqlite:///restaurant_menu.db')
